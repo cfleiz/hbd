6d8a8641e507fbf778ce7c800c9a30ec45770c4c
JUVENTUDESGTO\agodinez <agjuventudesgto@gmail.com>
2024-11-23 18:50:51 -0600

    Init comnit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const sobre = document.querySelector('.envelope');
+
+// Agrega el evento de clic al sobre
+sobre.addEventListener('click', function() {
+    // Alterna la clase 'open' para abrir el sobre y mostrar la carta
+    this.classList.toggle('open');
+});
+
+
